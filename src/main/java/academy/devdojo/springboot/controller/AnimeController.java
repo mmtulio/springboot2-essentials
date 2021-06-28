7484e1fac2f3ade6491d96a11bead141521a7a81
Tulio Muller <tmmuller@banestes.com.br>
2021-06-27 21:45:10 -0300

    aula 04: - movendo ApplicationStart para raiz do projeto; - criando DateUtil e adicionando em AnimeController; - adicionando dependência lombok; - adicionando log com @log42j - mudando para annotation @SpringBootApplication em ApplicationStart.

--- src/main/java/academy/devdojo/springboot/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -1,18 +1,27 @@
 package academy.devdojo.springboot.controller;
 
 import academy.devdojo.springboot.domain.Anime;
+import academy.devdojo.springboot.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@AllArgsConstructor
 public class AnimeController {
 
+    private DateUtil dateUtil;
+
     @GetMapping(path = "list")
-    public List<Anime> List(){
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("dbz"), new Anime("opm"));
 
     }
