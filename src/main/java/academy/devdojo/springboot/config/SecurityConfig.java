0cdfcd238cea472b8fce3a85abd20648409e068f
mmtulio <tmmull@gmail.com>
2021-07-24 23:48:37 -0300

    aula 43: - criada a classe DevDojoUser para representar o usuario; - criado o serviço DevDojoUserDetailsService que implementa a interface UserDetailsService; - criada a classe de repositório DevDojoUserRepository; - feitas modificações em SecurityConfig para possibilidar p login baseado nos dados do banco.

--- src/main/java/academy/devdojo/springboot/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package academy.devdojo.springboot.config;
 
+import academy.devdojo.springboot.service.DevDojoUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,7 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @Log4j2
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+@SuppressWarnings("java:S5344")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final DevDojoUserDetailsService devDojoUserDetailsService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf()
@@ -31,13 +36,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         log.info("Password encoded {}", passwordEncoder.encode("test"));
-        auth.inMemoryAuthentication()
-                .withUser("tulio")
-                .password(passwordEncoder.encode("test"))
-                .roles("ADMIN", "USER")
-                .and()
-                .withUser("tuleba")
-                .password(passwordEncoder.encode("test"))
-                .roles("USER");
+//        auth.inMemoryAuthentication()
+//                .withUser("tulio")
+//                .password(passwordEncoder.encode("test"))
+//                .roles("ADMIN", "USER")
+//                .and()
+//                .withUser("tuleba")
+//                .password(passwordEncoder.encode("test"))
+//                .roles("USER");
+
+        auth.userDetailsService(devDojoUserDetailsService).passwordEncoder(passwordEncoder);
     }
 }
